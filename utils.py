6209bf3c4524a170a1642ac745097d3fa14f4957
Takonan <takonan@gmail.com>
2015-11-27 00:56:18 -0500

    Modified the utils to load the training and validation set from the labeled data (num_example, num_dimension).

--- utils.py
+++ utils.py
@@ -1,12 +1,27 @@
 from scipy.io import loadmat
 import numpy as np
 
-def load_train():
+def load_data():
     data = loadmat('labeled_images.mat')
-    images = data['tr_images'].T # Transpose so the number of images is in first dimension
+    images = data['tr_images'].T # Transpose so the number of images is in first dimension: 2925, 32, 32
     targets = data['tr_labels']
     identities = data['tr_identity']
-    # train_inputs = np.vstack(images).reshape(images.shape[0], images.shape[1]*images.shape[2])
-    train_inputs = images.reshape(images.shape[0], images.shape[1]*images.shape[2])
-    train_targets = np.squeeze(data['tr_labels'])
-    return train_inputs, train_targets
+    # Flatten the 32x32 to 1024 1D
+    images = images.reshape(images.shape[0], images.shape[1]*images.shape[2])
+    # targets = np.squeeze(data['tr_labels'])
+
+    # Sort the array based on the tr_identities
+    # Sort the targets
+    temp = np.append(targets,identities,1)
+    targets_sort = temp[temp[:,1].argsort()]
+    num_unlabeled = sum(targets_sort[:,1] == -1)
+    valid_targets = targets_sort[0:num_unlabeled,0]
+    train_targets = targets_sort[(num_unlabeled + 1):,0]
+
+    # Sort the images
+    temp = np.append(images,identities,1)
+    images_sort = temp[temp[:,-1].argsort()]
+    valid_inputs = images_sort[0:num_unlabeled,0:-1] # Tested that the -1 omits the identity last column
+    train_inputs = images_sort[(num_unlabeled + 1):,0:-1]
+
+    return train_inputs, train_targets, valid_inputs, valid_targets
