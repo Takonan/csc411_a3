95543a397a36cef803718155435655dc1b69fa26
Takonan <takonan@gmail.com>
2015-12-03 12:08:32 -0500

    Updated the utils to include mirrored data and one of k encoding. Deeper MLP one

--- basicBoosting.py
+++ basicBoosting.py
@@ -2,26 +2,95 @@ from sklearn.cross_validation import cross_val_score
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVR
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import RidgeClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics.scorer import check_scoring
 from utils import *
+import time
 
-def run_AdaBoost():
-    train_inputs, train_targets = load_train()
-    clf = AdaBoostClassifier(n_estimators=100)
-    scores = cross_val_score(clf, train_inputs, train_targets)
+def run_AdaBoost(num_estimator=10, num_iter=5, include_mirror=False, do_cv=False):
+    train_inputs, train_targets, valid_inputs, valid_targets = load_data(include_mirror)
+    myClassifier = LogisticRegression()
+    # myClassifier = Perceptron(n_iter=num_iter)
+    # myClassifier = SGDClassifier(loss='perceptron',n_iter=num_iter)
+    clf = AdaBoostClassifier(base_estimator=myClassifier, n_estimators=num_estimator)
+
+    if do_cv:
+        # Do cross validation
+        scores = cross_val_score(clf, train_inputs, train_targets)
+        return scores.mean()
+
+    else:
+        # Do just one validation
+        clf.fit(train_inputs, train_targets)
+        pred = clf.predict(valid_inputs)
+        score = (pred == valid_targets).mean()
+        return score
+
+    # clf = AdaBoostClassifier(n_estimators=100)
+    # scores = cross_val_score(clf, train_inputs, train_targets, n_jobs=-1)
+    # print scores.mean()
+
+def run_ExtremeRandFor(include_mirror=False):
+    train_inputs, train_targets, valid_inputs, valid_targets = load_data(include_mirror)
+    clf = ExtraTreesClassifier(n_estimators=100, max_depth=None, min_samples_split=1, random_state=0, n_jobs=-1)
+    scores = cross_val_score(clf, train_inputs, train_targets, n_jobs=-1)
     print scores.mean()
 
-def run_ExtremeRandFor():
-    train_inputs, train_targets = load_train()
-    clf = ExtraTreesClassifier(n_estimators=100, max_depth=None, min_samples_split=1, random_state=0)
-    scores = cross_val_score(clf, train_inputs, train_targets)
+def run_RandFor(include_mirror=False):
+    train_inputs, train_targets, valid_inputs, valid_targets = load_data(include_mirror)
+    clf = RandomForestClassifier(n_estimators=100, max_depth=None, min_samples_split=1, random_state=0, n_jobs=-1)
+    scores = cross_val_score(clf, train_inputs, train_targets, n_jobs=-1)
     print scores.mean()
 
-def run_ExtremeRandFor():
-    train_inputs, train_targets = load_train()
-    clf = RandomForestClassifier(n_estimators=10, max_depth=None, min_samples_split=1, random_state=0)
-    scores = cross_val_score(clf, train_inputs, train_targets)
-    print scores.mean()
+def run_Bagging(num_estimator=10, num_iter=5, include_mirror=False, do_cv=False):
+    train_inputs, train_targets, valid_inputs, valid_targets = load_data(include_mirror)
+    # myClassifier = LinearSVC()
+    # myClassifier = RidgeClassifier()
+    # myClassifier = Perceptron(n_iter=num_iter)
+    myClassifier = SGDClassifier(loss='perceptron',n_iter=num_iter)
+    clf = BaggingClassifier(base_estimator=myClassifier, n_estimators=num_estimator, n_jobs=-1)
 
+    if do_cv:
+        # Do cross validation
+        scores = cross_val_score(clf, train_inputs, train_targets)
+        return scores.mean()
+
+    else:
+        # Do just one validation
+        clf.fit(train_inputs, train_targets)
+        pred = clf.predict(valid_inputs)
+        score = (pred == valid_targets).mean()
+        return score
+
+    return
 
 if __name__ == '__main__':
-    run_AdaBoost()
+    # print "Running classification algorithms with original training data set:"
+    # # run_AdaBoost()
+    # # run_ExtremeRandFor()
+    # # run_RandFor()
+    # start = time.time()
+    # run_Bagging()
+    # elasped = time.time() - start
+    # print "Elasped time: ", elasped
+    # print "Running classification algorithms with original training data set and mirrorred data set:"
+    # # run_AdaBoost(True)
+    # # run_ExtremeRandFor(True)
+    # # run_RandFor(True)
+    # start = time.time()
+    # run_Bagging(True)
+    # elasped = time.time() - start
+    # print "Elasped time: ", elasped
+
+
+    for num_estimator in [25]: #[10, 25, 50]:
+        for num_iter in [5]: #[5, 10, 25, 50]:
+            print "Original Set, num_estimator: %d, num_iter: %d, accuracy: %f" % (num_estimator, num_iter, run_AdaBoost(num_estimator, num_iter, False, False))
+            print "Original + Mirrored Set, num_estimator: %d, num_iter: %d, accuracy: %f" % (num_estimator, num_iter, run_AdaBoost(num_estimator, num_iter, True, False))
--- utils.py
+++ utils.py
@@ -1,11 +1,22 @@
 from scipy.io import loadmat
 import numpy as np
 
-def load_data():
+def load_data(include_mirror=False):
     data = loadmat('labeled_images.mat')
     images = data['tr_images'].T # Transpose so the number of images is in first dimension: 2925, 32, 32
     targets = data['tr_labels']
     identities = data['tr_identity']
+
+    # Generate a mirrored version if necessary:
+    if include_mirror:
+        mirrored_faces = np.transpose(images, (0,2,1))
+        rotated_faces = mirrored_faces[:,:,::-1]
+        images = np.append(rotated_faces, mirrored_faces, 0)
+        identities = np.append(identities, identities,0)
+        targets = np.append(targets, targets,0)
+    else:
+        images = np.transpose(images, (0,2,1))[:,:,::-1]
+
     # Flatten the 32x32 to 1024 1D
     images = images.reshape(images.shape[0], images.shape[1]*images.shape[2])
     # targets = np.squeeze(data['tr_labels'])
@@ -26,11 +37,78 @@ def load_data():
 
     return train_inputs, train_targets, valid_inputs, valid_targets
 
-def load_data_with_test():
+def load_data_with_test(include_mirror=False):
     data = loadmat('labeled_images.mat')
     images = data['tr_images'].T # Transpose so the number of images is in first dimension: 2925, 32, 32
     targets = data['tr_labels']
     identities = data['tr_identity']
+
+    # Generate a mirrored version if necessary:
+    if include_mirror:
+        mirrored_faces = np.transpose(images, (0,2,1))
+        rotated_faces = mirrored_faces[:,:,::-1]
+        images = np.append(rotated_faces, mirrored_faces, 0)
+        identities = np.append(identities, identities,0)
+        targets = np.append(targets, targets,0)
+    else:
+        images = np.transpose(images, (0,2,1))[:,:,::-1]
+
+    # Flatten the 32x32 to 1024 1D
+    images = images.reshape(images.shape[0], images.shape[1]*images.shape[2])
+    # targets = np.squeeze(data['tr_labels'])
+
+    # # Sort the array based on the tr_identities
+    # # Sort the targets
+    # temp = np.append(targets,identities,1)
+    # targets_sort = temp[temp[:,1].argsort()]
+    # num_unlabeled = sum(targets_sort[:,1] == -1)
+    # valid_targets = targets_sort[0:num_unlabeled,0]
+    # test_targets = targets_sort[(num_unlabeled + 1):2*num_unlabeled,0]
+    # train_targets = targets_sort[(2*num_unlabeled + 1):,0]
+
+    # # Sort the images
+    # temp = np.append(images,identities,1)
+    # images_sort = temp[temp[:,-1].argsort()]
+    # valid_inputs = images_sort[0:num_unlabeled,0:-1] # Tested that the -1 omits the identity last column
+    # test_inputs = images_sort[(num_unlabeled + 1):2*num_unlabeled,0:-1]
+    # train_inputs = images_sort[(2*num_unlabeled + 1):,0:-1]
+
+    # DEBUG: Only use half of the unlabeled identity for validation
+    # Sort the array based on the tr_identities
+    # Sort the targets
+    temp = np.append(targets,identities,1)
+    targets_sort = temp[temp[:,1].argsort()]
+    num_unlabeled = sum(targets_sort[:,1] == -1)
+    valid_targets = targets_sort[0:num_unlabeled/2,0]
+    test_targets = targets_sort[(num_unlabeled/2 + 1):num_unlabeled,0]
+    train_targets = targets_sort[(num_unlabeled + 1):,0]
+
+    # Sort the images
+    temp = np.append(images,identities,1)
+    images_sort = temp[temp[:,-1].argsort()]
+    valid_inputs = images_sort[0:num_unlabeled/2,0:-1] # Tested that the -1 omits the identity last column
+    test_inputs = images_sort[(num_unlabeled/2 + 1):num_unlabeled,0:-1]
+    train_inputs = images_sort[(num_unlabeled + 1):,0:-1]
+    # End debug
+
+    return train_inputs, train_targets, valid_inputs, valid_targets, test_inputs, test_targets
+
+def load_data_one_of_k(include_mirror=False):
+    data = loadmat('labeled_images.mat')
+    images = data['tr_images'].T # Transpose so the number of images is in first dimension: 2925, 32, 32
+    targets = data['tr_labels']
+    identities = data['tr_identity']
+
+    # Generate a mirrored version if necessary:
+    if include_mirror:
+        mirrored_faces = np.transpose(images, (0,2,1))
+        rotated_faces = mirrored_faces[:,:,::-1]
+        images = np.append(rotated_faces, mirrored_faces, 0)
+        identities = np.append(identities, identities,0)
+        targets = np.append(targets, targets,0)
+    else:
+        images = np.transpose(images, (0,2,1))[:,:,::-1]
+
     # Flatten the 32x32 to 1024 1D
     images = images.reshape(images.shape[0], images.shape[1]*images.shape[2])
     # targets = np.squeeze(data['tr_labels'])
@@ -40,15 +118,64 @@ def load_data_with_test():
     temp = np.append(targets,identities,1)
     targets_sort = temp[temp[:,1].argsort()]
     num_unlabeled = sum(targets_sort[:,1] == -1)
-    valid_targets = targets_sort[0:num_unlabeled,0]
-    test_targets = targets_sort[(num_unlabeled + 1):2*num_unlabeled,0]
-    train_targets = targets_sort[(2*num_unlabeled + 1):,0]
+
+    temp = np.zeros((temp.shape[0], 7))
+    for i in range(temp.shape[0]):
+        temp[i][targets_sort[i,0]-1] = 1
+
+    valid_targets = temp[0:num_unlabeled,:]
+    train_targets = temp[(num_unlabeled+1):,:]
 
     # Sort the images
     temp = np.append(images,identities,1)
     images_sort = temp[temp[:,-1].argsort()]
     valid_inputs = images_sort[0:num_unlabeled,0:-1] # Tested that the -1 omits the identity last column
-    test_inputs = images_sort[(num_unlabeled + 1):2*num_unlabeled,0:-1]
-    train_inputs = images_sort[(2*num_unlabeled + 1):,0:-1]
+    train_inputs = images_sort[(num_unlabeled + 1):,0:-1]
+
+    return train_inputs, train_targets, valid_inputs, valid_targets
+
+
+def load_data_with_test_one_of_k(include_mirror=False):
+    data = loadmat('labeled_images.mat')
+    images = data['tr_images'].T # Transpose so the number of images is in first dimension: 2925, 32, 32
+    targets = data['tr_labels']
+    identities = data['tr_identity']
+
+    # Generate a mirrored version if necessary:
+    if include_mirror:
+        mirrored_faces = np.transpose(images, (0,2,1))
+        rotated_faces = mirrored_faces[:,:,::-1]
+        images = np.append(rotated_faces, mirrored_faces, 0)
+        identities = np.append(identities, identities,0)
+        targets = np.append(targets, targets,0)
+    else:
+        images = np.transpose(images, (0,2,1))[:,:,::-1]
+
+    # Flatten the 32x32 to 1024 1D
+    images = images.reshape(images.shape[0], images.shape[1]*images.shape[2])
+    # targets = np.squeeze(data['tr_labels'])
+
+    # DEBUG: Only use half of the unlabeled identity for validation
+    # Sort the array based on the tr_identities
+    # Sort the targets
+    temp = np.append(targets,identities,1)
+    targets_sort = temp[temp[:,1].argsort()]
+    num_unlabeled = sum(targets_sort[:,1] == -1)
+
+    temp = np.zeros((temp.shape[0], 7))
+    for i in range(temp.shape[0]):
+        temp[i][targets_sort[i,0]-1] = 1
+
+    valid_targets = temp[0:num_unlabeled/2,:]
+    test_targets = temp[(num_unlabeled/2+1):num_unlabeled,:]
+    train_targets = temp[(num_unlabeled+1):,:]
+
+    # Sort the images
+    temp = np.append(images,identities,1)
+    images_sort = temp[temp[:,-1].argsort()]
+    valid_inputs = images_sort[0:num_unlabeled/2,0:-1] # Tested that the -1 omits the identity last column
+    test_inputs = images_sort[(num_unlabeled/2 + 1):num_unlabeled,0:-1]
+    train_inputs = images_sort[(num_unlabeled + 1):,0:-1]
+    # End debug
 
     return train_inputs, train_targets, valid_inputs, valid_targets, test_inputs, test_targets
